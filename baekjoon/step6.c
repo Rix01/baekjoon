0cf9b1b2932744a84e5264374c2324e84d131420
Lee Chae Rin <chaerin0916@gmail.com>
2022-04-27 13:18:48 +0900

    추가

--- baekjoon/step6.c
+++ baekjoon/step6.c
@@ -165,3 +165,92 @@ int main(void)
 }
 */
 
+// 2908
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	char num1[4]; // 문자열 배열의 경우, 마지막 null자리가 있어야 함.
+	char num2[4];
+
+	scanf("%s %s", num1, num2);
+
+	// num1 재배열
+	char temp = num1[0];
+	num1[0] = num1[2];
+	num1[2] = temp;
+
+	// num2 재배열
+	temp = num2[0];
+	num2[0] = num2[2];
+	num2[2] = temp;
+
+	int n1 = ((num1[0] - 48) * 100) + ((num1[1] - 48) * 10) + (num1[2] - 48);
+	int n2 = ((num2[0] - 48) * 100) + ((num2[1] - 48) * 10) + (num2[2] - 48);
+
+	if (n1 > n2) printf("%d", n1);
+	else printf("%d", n2);
+}
+*/
+
+// 5622
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char num[16];
+	scanf("%s", num);
+	int len = strlen(num);
+	int cho, sum=0;
+
+	for (int i = 0; i < len; i++)
+	{
+		if ((num[i] == 'A') || (num[i] == 'B') || (num[i] == 'C'))
+		{
+			cho = 3;
+			sum += cho;
+		}
+		else if ((num[i] == 'D') || (num[i] == 'E') || (num[i] == 'F'))
+		{
+			cho = 4;
+			sum += cho;
+		}
+		else if ((num[i] == 'G') || (num[i] == 'H') || (num[i] == 'I'))
+		{
+			cho = 5;
+			sum += cho;
+		}
+		else if ((num[i] == 'J') || (num[i] == 'K') || (num[i] == 'L'))
+		{
+			cho = 6;
+			sum += cho;
+		}
+		else if ((num[i] == 'M') || (num[i] == 'N') || (num[i] == 'O'))
+		{
+			cho = 7;
+			sum += cho;
+		}
+		else if ((num[i] == 'P') || (num[i] == 'Q') || (num[i] == 'R')|| (num[i] == 'S'))
+		{
+			cho = 8;
+			sum += cho;
+		}
+		else if ((num[i] == 'T') || (num[i] == 'U') || (num[i] == 'V'))
+		{
+			cho = 9;
+			sum += cho;
+		}
+		else if ((num[i] == 'W') || (num[i] == 'X') || (num[i] == 'Y')|| (num[i] == 'Z'))
+		{
+			cho = 10;
+			sum += cho;
+		}
+	}
+	printf("%d", sum);
+	return 0;
+}
+*/
+
